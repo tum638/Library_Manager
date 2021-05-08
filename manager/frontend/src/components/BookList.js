1ae7705e1fa8e485fdd7e723818c30c0a94e7bbd
tinotendakanyoka <purplelaneofficial@gmail.com>
2021-05-08 18:58:09 +0200

    .

--- manager/frontend/src/components/BookList.js
+++ manager/frontend/src/components/BookList.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { DataGrid } from '@material-ui/data-grid';
-
+import { Button } from '@material-ui/core';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 const useStyles = makeStyles({
   table: {
     minWidth: 650,
@@ -21,25 +27,50 @@ class BookList extends Component{
 
   }
 
-componentDidMount() {
+  fetchBooks = () => {
     fetch('/api/books')
     .then((response) => response.json())
     .then(booksList => {
         this.setState({ books: booksList });
     });
-}
 
+    function createData(name, calories, fat, carbs, protein) {
+      return { name, calories, fat, carbs, protein };
+    }
+}
 
 
   render(){
 
     return (
       <div>
-      <ul>
-      {this.state.books.map((book) => (
-          <li key={book.id}>{book.title}</li>
-      ))}
-  </ul>
+        <Button onClick={this.fetchBooks} variant='outlined'>Load Books</Button>
+  <TableContainer component={Paper}>
+      <Table aria-label="books table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Book Title</TableCell>
+            <TableCell align="right">Subject</TableCell>
+            <TableCell align="right">Publisher</TableCell>
+            <TableCell align="right">Country</TableCell>
+            <TableCell align="right">Borrowed</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {this.state.books.map((book) => (
+            <TableRow key={book.id}>
+              <TableCell component="th" scope="row">
+                {book.title}
+              </TableCell>
+              <TableCell align="right">{book.subject}</TableCell>
+              <TableCell align="right">{book.publisher}</TableCell>
+              <TableCell align="right">{book.country}</TableCell>
+              <TableCell align="right">{book.borrowed.toString()}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   </div>
     )
 
