03e9173ffb16e47d04823857337041cc85ec2107
tinotendakanyoka <purplelaneofficial@gmail.com>
2021-05-11 16:54:36 +0200

    Update autocomplete function

--- manager/frontend/src/components/Borrow.js
+++ manager/frontend/src/components/Borrow.js
@@ -1,54 +1,83 @@
-import React, { useState } from 'react'
-import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
-import { Autocomplete } from '@material-ui/lab';
-import { Grid, Typography, Button } from '@material-ui/core';
-import ScanButton from './ScanButton';
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    '& > *': {
-      margin: theme.spacing(1),
-      width: '25ch',
-    },
-  },
-
-  heading: {
-      color: "#3f51b5"
-  },
-
-  button: {
-      background: "#3f51b5"
-  }
+import React, { Component } from 'react';
+import Autocomplete from './Autocomplete';
 
 
-}));
+class Borrow extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            books: [],
+
+         };
 
 
-const Borrow = () => {
-    const classes = useStyles();
+    }
 
+    fetchBooks = () => {
+        fetch('/api/books')
+        .then((response) => response.json())
+        .then(booksList => {
+            this.setState({ books: booksList });
+        });
+    
+    }
 
-    return (
-        <div>
+    componentDidMount() {
+        this.fetchBooks();
+    }
+
+    render() {
+        return (
+            <div>
             <Grid container direction="column">
               <Grid item xs={12} alignItems="center">
-                  <Typography variant="h3" className={classes.heading}>Borrow Book</Typography>
+                  <Typography variant="h3" className={classes.heading}>Return Book</Typography>
               </Grid>
               <Grid item xs={12}>
     <form className={classes.root} noValidate autoComplete="off">
-
+      <Autocomplete
+      suggestions = {this.state.books.map((book) => (
+          book.title
+      ))}
+    />
+    <Button color="primary" size="small">search by subject</Button>
+          <Autocomplete
+      id="country-select-demo"
+      style={{ width: 260 }}
+      classes={{
+        option: classes.option,
+      }}
+      autoHighlight
+      getOptionLabel={(option) => option.label}
+      renderOption={(option) => (
+        <React.Fragment>
+          {option.label} ({option.code})
+        </React.Fragment>
+      )}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          label="Student Ref Code"
+          inputProps={{
+            ...params.inputProps,
+            autoComplete: 'new-password', // disable autocomplete and autofill
+          }}
+        />
+      )}
+    /><Button color="primary" size="small">search by name</Button>
     </form>
     <Grid item container direction="row" xs={12} className={classes.root}>
 
         <Grid item>
 
         <Button variant="outlined">
-            Borrow
+            Return
         </Button>
         </Grid>
         <Grid item>
-          <ScanButton />
+            <Button variant="contained" color="primary">
+                Scan
+            </Button>
         </Grid>
 
     </Grid>
@@ -56,7 +85,8 @@ const Borrow = () => {
             </Grid>
             
         </div>
-    )
+        );
+    }
 }
 
-export default Borrow
+export default Borrow;
