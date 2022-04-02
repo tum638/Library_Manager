a58fc0525b30bf6f8a30849ed7ba541eed24e2a6
Tinotenda Kanyoka <k22196@sgc.ac.zw>
2022-04-02 13:09:12 +0200

    Views and Models

--- manager/api/urls.py
+++ manager/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import entry, BooksList, BorrowsList
+from .views import entry, BooksList, BorrowsList, borrow
 from .lookup import get_book, get_from_db
 
 urlpatterns = [
     path('entry/', entry),
     path('books/', BooksList.as_view()),
-    path('borrows', BorrowsList.as_view())
+    path('borrows-list', BorrowsList.as_view()),
+    path('borrow', borrow)
 
 ]
--- manager/api/views.py
+++ manager/api/views.py
@@ -26,3 +26,20 @@ def entry(request):
 def delete(request):
     book = Book.objects.get(id=request.POST['id'])
     book.delete()
+    
+def borrow(request):
+    student_name = request.POST['student_name']
+    book_id = request.POST['bookid']
+    
+    book = Book.objects.get(id=book_id)
+    student = Student.objects.get(fullname=student_name)
+    
+    student.total_books += 1
+    book.borrowed = True
+    
+    student.save()
+    book.save()
+    
+    borrow = Borrow(book=book, student=student)
+    
+    borrow.save()
