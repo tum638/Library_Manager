ed3773d533cd13726002f5b3cd22d1288a76c64e
Tinotenda Kanyoka <k22196@sgc.ac.zw>
2022-04-13 10:02:35 +0200

    scanner and lookup utils

--- Backend/api/models.py
+++ Backend/api/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=256)
+    isbn = models.CharField(max_length=13)
+    author = models.CharField(max_length=128)
+    description = models.TextField(default="Book Description", null=True, blank=True)
+    publisher = models.CharField(max_length=200, null=True, blank=True)
+    year = models.CharField(max_length=4, null=True, blank=True)
+    in_library = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return self.title
+    
+    def borrow(self):
+        if self.in_library:
+            self.in_library = False
+            self.save()
+            return True
+        else:
+            return False
+        
+    def return_book(self):
+        if not self.in_library:
+            self.in_library = True
+            self.save()
+            return True
+        else:
+            return False
+        
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    number_of_books_read = models.IntegerField(default=0)
+    current_book = models.ForeignKey(Book, on_delete=models.DO_NOTHING, null=True, blank=True)
+    
+    def __str__(self):
+        return self.name
+        
+    
---
+++ Backend/api/utils.py
@@ -0,0 +1,43 @@
+import cv2
+from pyzbar.pyzbar import decode
+from isbnlib import meta, cover, desc
+from isbnlib.dev._exceptions import DataNotFoundAtServiceError
+
+def scanner():
+    cap = cv2.VideoCapture(0)
+    a = True
+    
+    while a:
+        success, frame = cap.read()
+        
+        for code in decode(frame):
+            data = code.data.decode('utf-8')
+            if len(data) != 0:
+                return data
+                a = False
+                break
+        cv2.waitKey(1)
+
+def lookup(code):
+    try:
+        book_data = meta(code, service='openl')
+    except DataNotFoundAtServiceError:
+        book_data = {}
+        
+    if len(book_data) == 0:
+        print("book cannot be added")
+        
+        #description = desc(code)
+        #cover_url = cover(code)
+        #print(book_data)
+        #print(description)
+        #print(cover_url)
+    return book_data
+        
+        
+
+        
+    
+
+    
+lookup(scanner())
