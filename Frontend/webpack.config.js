dae6051d34bf26203d5ba49ebc55db48a260bbdc
Tinotenda Kanyoka <k22196@sgc.ac.zw>
2022-04-30 14:46:26 +0200

    Error handling

--- Frontend/webpack.config.js
+++ Frontend/webpack.config.js
@@ -14,7 +14,12 @@ const htmlPlugin = new HtmlWebPackPlugin({
   {
    test: /\.css$/,
    use: ['style-loader', 'css-loader']
-  }
+  },
+  {
+    test: /\.js$/,
+    enforce: 'pre',
+    use: ['source-map-loader'],
+  },
 ]},
  plugins: [htmlPlugin]
 };
